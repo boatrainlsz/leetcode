635599204696bee6d0fb794568e9b762818c3b44
boatrain <yinger521999@gmail.com>
2020-12-15 18:07:44 +0800

    单调递增数字

---
+++ src/com/boatrain/numbers/MonotoneIncreasingDigits.java
@@ -0,0 +1,17 @@
+package com.boatrain.numbers;
+
+/**
+ * https://leetcode-cn.com/problems/monotone-increasing-digits/
+ */
+public class MonotoneIncreasingDigits {
+    public static void main(String[] args) {
+        MonotoneIncreasingDigits solution = new MonotoneIncreasingDigits();
+        System.out.println(solution.monotoneIncreasingDigits(332));
+    }
+
+    public int monotoneIncreasingDigits(int N) {
+        int ans = 0;
+        
+        return ans;
+    }
+}
