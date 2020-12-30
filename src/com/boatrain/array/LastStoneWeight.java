698ab25fcb73af729c05acc5545847994d8d2326
boatrain <yinger521999@gmail.com>
2020-12-30 21:16:00 +0800

    最后一块石头的重量

---
+++ src/com/boatrain/array/LastStoneWeight.java
@@ -0,0 +1,29 @@
+package com.boatrain.array;
+
+import java.util.PriorityQueue;
+
+public class LastStoneWeight {
+    public static void main(String[] args) {
+        LastStoneWeight solution = new LastStoneWeight();
+        System.out.println(solution.lastStoneWeight(new int[]{2, 7, 4, 1, 8, 1}));
+    }
+
+    public int lastStoneWeight(int[] stones) {
+        PriorityQueue<Integer> maxHeap = new PriorityQueue<Integer>(
+                (o1, o2) -> o2 - o1);
+        for (int stone : stones) {
+            maxHeap.add(stone);
+        }
+        while (maxHeap.size() > 0) {
+            Integer max = maxHeap.poll();
+            if (maxHeap.size() == 0) {
+                return max;
+            }
+            Integer secondMax = maxHeap.poll();
+            if (!secondMax.equals(max)) {
+                maxHeap.add(max - secondMax);
+            }
+        }
+        return 0;
+    }
+}
