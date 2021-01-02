cd1bc976f699bad2513eeea0d1e6f10b043ad39c
boatrain <yinger521999@gmail.com>
2021-01-02 21:21:06 +0800

    滑动窗口最大值

--- src/com/boatrain/temp/Solution.java
+++ src/com/boatrain/temp/Solution.java
@@ -1,26 +1,28 @@
 package com.boatrain.temp;
 
+import java.util.Arrays;
+import java.util.PriorityQueue;
+
 class Solution {
     public static void main(String[] args) {
         Solution solution = new Solution();
-        int[] nums = {10, 9, 2, 5, 3, 7, 101, 18};
-//        int[] nums = {0, 1, 0, 3, 2, 3};
-        System.out.println(solution.lengthOfLIS(nums));
+        System.out.println(Arrays.toString(solution.maxSlidingWindow(new int[]{1, 3, -1, -3, 5, 3, 6, 7}, 3)));
     }
 
-    public int lengthOfLIS(int[] nums) {
-        int[] dp = new int[nums.length];
-        int ans = 1;
-        int len = nums.length;
-        dp[0] = 1;
-        for (int i = 1; i < len; i++) {
-            dp[i] =1;
-            for (int j = 0; j < i; j++) {
-                if (nums[j] < nums[i]) {
-                    dp[i] = Math.max(dp[i], dp[j]+1);
-                }
+    public int[] maxSlidingWindow(int[] nums, int k) {
+        int n = nums.length;
+        int[] ans = new int[n - k + 1];
+        PriorityQueue<int[]> maxHeap = new PriorityQueue<>((o1, o2) -> o2[0] - o1[0]);
+        for (int i = 0; i < k; i++) {
+            maxHeap.add(new int[]{nums[i], i});
+        }
+        ans[0] = maxHeap.peek()[0];
+        for (int i = k; i < n; i++) {
+            maxHeap.add(new int[]{nums[i], i});
+            while (!maxHeap.isEmpty() && maxHeap.peek()[1] <= i - k) {
+                maxHeap.poll();
             }
-            ans = Math.max(ans, dp[i]);
+            ans[i - k + 1] = maxHeap.peek()[0];
         }
         return ans;
     }
