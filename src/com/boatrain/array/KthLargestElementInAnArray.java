e73d111b82fde3b7aa0ad5e29556449cd69311c2
boatrain <yinger521999@gmail.com>
2021-01-02 21:43:59 +0800

    第k大的数，非递归实现

--- src/com/boatrain/array/KthLargestElementInAnArray.java
+++ src/com/boatrain/array/KthLargestElementInAnArray.java
@@ -9,7 +9,7 @@ public class KthLargestElementInAnArray {
     public static void main(String[] args) {
         KthLargestElementInAnArray solution = new KthLargestElementInAnArray();
         int[] nums = new int[]{3, 2, 1, 5, 6, 4};
-        System.out.println(solution.findKthLargest(nums, 2));
+        System.out.println(solution.findKthLargestNonR(nums, 2));
     }
 
     /**
@@ -20,7 +20,27 @@ public class KthLargestElementInAnArray {
      * @param random 随机
      * @return arr[l, r]中第k小的元素，k从0开始计数
      */
-    private static int partition(int[] arr, int l, int r, int k, Random random) {
+    private static int selectK(int[] arr, int l, int r, int k, Random random) {
+        int j = partition(arr, l, r, random);
+        if (j == k) {
+            return arr[k];
+        } else if (j < k) {
+            return selectK(arr, j + 1, r, k, random);
+        } else {
+            return selectK(arr, l, j - 1, k, random);
+        }
+    }
+
+    /**
+     * 快排里的partition函数
+     *
+     * @param arr
+     * @param l
+     * @param r
+     * @param random
+     * @return
+     */
+    private static int partition(int[] arr, int l, int r, Random random) {
         int p = random.nextInt(r - l + 1) + l;
         //交换一下，再开始分区
         swap(arr, l, p);
@@ -43,13 +63,7 @@ public class KthLargestElementInAnArray {
             j--;
         }
         swap(arr, l, j);
-        if (j == k) {
-            return arr[k];
-        } else if (j < k) {
-            return partition(arr, j + 1, r, k, random);
-        } else {
-            return partition(arr, l, j - 1, k, random);
-        }
+        return j;
     }
 
     private static void swap(int[] arr, int i, int j) {
@@ -58,10 +72,41 @@ public class KthLargestElementInAnArray {
         arr[j] = temp;
     }
 
+    /**
+     * 递归实现
+     *
+     * @param nums
+     * @param k
+     * @return
+     */
     public int findKthLargest(int[] nums, int k) {
         Random random = new Random();
         int n = nums.length;
-        return partition(nums, 0, nums.length - 1, n - k, random);
+        return selectK(nums, 0, nums.length - 1, n - k, random);
+    }
+
+    /**
+     * 非递归实现
+     *
+     * @param nums
+     * @param k
+     * @return
+     */
+    public int findKthLargestNonR(int[] nums, int k) {
+        int n = nums.length;
+        int l = 0, r = nums.length - 1, target = n - k;
+        Random random = new Random();
+        while (l <= r) {
+            int j = partition(nums, l, r, random);
+            if (j == target) {
+                return nums[j];
+            } else if (j < target) {
+                l = j + 1;
+            } else {
+                r = j - 1;
+            }
+        }
+        return -1;
     }
 
 }
