b8ae416c36691f96ea65996be3cb72c1ac07e728
boatrain <yinger521999@gmail.com>
2021-01-03 15:21:04 +0800

    将数组分成三个子数组的方案数

---
+++ src/com/boatrain/array/WaysToSplitArrayIntoThreeSubarrays.java
@@ -0,0 +1,16 @@
+package com.boatrain.array;
+
+/**
+ * https://leetcode-cn.com/problems/ways-to-split-array-into-three-subarrays/
+ */
+public class WaysToSplitArrayIntoThreeSubarrays {
+    public static void main(String[] args) {
+        WaysToSplitArrayIntoThreeSubarrays solution = new WaysToSplitArrayIntoThreeSubarrays();
+        int[] nums = new int[]{1, 2, 2, 2, 5, 0};
+        System.out.println(solution.waysToSplit(nums));
+    }
+
+    public int waysToSplit(int[] nums) {
+        return 1;
+    }
+}
