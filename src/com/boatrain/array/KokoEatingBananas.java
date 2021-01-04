19b8bc0419f756314343e973d5f99a3afd4652d3
boatrain <yinger521999@gmail.com>
2021-01-04 09:15:12 +0800

    猴子吃香蕉

--- src/com/boatrain/array/KokoEatingBananas.java
+++ src/com/boatrain/array/KokoEatingBananas.java
@@ -4,6 +4,12 @@ package com.boatrain.array;
  * https://leetcode-cn.com/problems/koko-eating-bananas/
  */
 public class KokoEatingBananas {
+    public static void main(String[] args) {
+        KokoEatingBananas solution = new KokoEatingBananas();
+        int[] piles = new int[]{30, 11, 23, 4, 20};
+        System.out.println(solution.minEatingSpeed(piles, 8));
+    }
+
     public int minEatingSpeed(int[] piles, int H) {
         int l = 1;
         int r = 0;
@@ -15,16 +21,19 @@ public class KokoEatingBananas {
             int mid = l + ((r - l) >> 1);
             int sum = 0;
             for (int pile : piles) {
-                int temp = pile / mid;
-                int mod = pile % mid;
-                if (mod != 0) {
-                    temp++;
+                //将quotient向上取整
+                int quotient = pile / mid;
+                int remainder = pile % mid;
+                if (remainder != 0) {
+                    quotient++;
                 }
-                sum += temp;
+                sum += quotient;
             }
             if (sum > H) {
+                //用mid速度吃，还是太慢了
                 l = mid + 1;
             } else {
+                //用mid速度可以满足，尝试能不能找到更小的速度
                 r = mid - 1;
             }
         }
