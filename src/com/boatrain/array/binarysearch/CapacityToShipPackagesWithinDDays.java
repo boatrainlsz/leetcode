ce57b32041fd9ce6c84e8e67ae7580f203dfaeff
boatrain <yinger521999@gmail.com>
2021-01-04 21:02:32 +0800

    二分查找

---
+++ src/com/boatrain/array/binarysearch/CapacityToShipPackagesWithinDDays.java
@@ -0,0 +1,18 @@
+package com.boatrain.array.binarysearch;
+
+/**
+ * https://leetcode-cn.com/problems/capacity-to-ship-packages-within-d-days/
+ */
+public class CapacityToShipPackagesWithinDDays {
+    public static void main(String[] args) {
+        CapacityToShipPackagesWithinDDays solution = new CapacityToShipPackagesWithinDDays();
+        int[] weights = new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        int D = 5;
+        System.out.println(solution.shipWithinDays(weights, D));
+    }
+
+    public int shipWithinDays(int[] weights, int D) {
+
+        return 1;
+    }
+}
