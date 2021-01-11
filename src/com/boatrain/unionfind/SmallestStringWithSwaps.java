9a0845b4f722e04de5a12b940a280a39ea27055b
boatrain <yinger521999@gmail.com>
2021-01-11 14:21:21 +0800

    SmallestStringWithSwaps

---
+++ src/com/boatrain/unionfind/SmallestStringWithSwaps.java
@@ -0,0 +1,108 @@
+package com.boatrain.unionfind;
+
+import com.boatrain.dynamicprogramming.Solution;
+
+import java.util.*;
+
+/**
+ * https://leetcode-cn.com/problems/smallest-string-with-swaps/
+ * 考点：并查集
+ */
+public class SmallestStringWithSwaps {
+    public static void main(String[] args) {
+        SmallestStringWithSwaps solution = new SmallestStringWithSwaps();
+        String str = "dcab";
+        int[][] pairs = new int[][]{{0, 3}, {1, 2}, {0, 2}};
+        List<List<Integer>> list = new ArrayList<>();
+        for (int[] pair : pairs) {
+            List<Integer> temp = new ArrayList<>();
+            temp.add(pair[0]);
+            temp.add(pair[1]);
+            list.add(temp);
+        }
+        System.out.println(solution.smallestStringWithSwaps(str, list));
+    }
+
+    public String smallestStringWithSwaps(String s, List<List<Integer>> pairs) {
+        if (pairs.size() == 0) {
+            return s;
+        }
+
+        // 第 1 步：将任意交换的结点对输入并查集
+        int len = s.length();
+        UnionFind unionFind = new UnionFind(len);
+        for (List<Integer> pair : pairs) {
+            unionFind.union(pair.get(0), pair.get(1));
+        }
+
+        // 第 2 步：构建映射关系
+        char[] charArray = s.toCharArray();
+        // key：连通分量的代表元，value：同一个连通分量的字符集合（保存在一个最小堆中）
+        Map<Integer, PriorityQueue<Character>> hashMap = new HashMap<>(len);
+        for (int i = 0; i < len; i++) {
+            int root = unionFind.find(i);
+            if (hashMap.containsKey(root)) {
+                hashMap.get(root).offer(charArray[i]);
+            } else {
+                PriorityQueue<Character> minHeap = new PriorityQueue<>();
+                minHeap.offer(charArray[i]);
+                hashMap.put(root, minHeap);
+                // 上面三行代码等价于下面一行代码，JDK 1.8 以及以后支持下面的写法
+                hashMap.computeIfAbsent(root, key -> new PriorityQueue<>()).offer(charArray[i]);
+            }
+        }
+
+        // 第 3 步：重组字符串
+        StringBuilder stringBuilder = new StringBuilder();
+        for (int i = 0; i < len; i++) {
+            int root = unionFind.find(i);
+            stringBuilder.append(hashMap.get(root).poll());
+        }
+        return stringBuilder.toString();
+    }
+
+    private class UnionFind {
+
+        private int[] parent;
+        /**
+         * 以 i 为根结点的子树的高度（引入了路径压缩以后该定义并不准确）
+         */
+        private int[] rank;
+
+        public UnionFind(int n) {
+            this.parent = new int[n];
+            this.rank = new int[n];
+            for (int i = 0; i < n; i++) {
+                this.parent[i] = i;
+                this.rank[i] = 1;
+            }
+        }
+
+        public void union(int x, int y) {
+            int rootX = find(x);
+            int rootY = find(y);
+            if (rootX == rootY) {
+                return;
+            }
+
+            if (rank[rootX] == rank[rootY]) {
+                parent[rootX] = rootY;
+                // 此时以 rootY 为根结点的树的高度仅加了 1
+                rank[rootY]++;
+            } else if (rank[rootX] < rank[rootY]) {
+                parent[rootX] = rootY;
+                // 此时以 rootY 为根结点的树的高度不变
+            } else {
+                // 同理，此时以 rootX 为根结点的树的高度不变
+                parent[rootY] = rootX;
+            }
+        }
+
+        public int find(int x) {
+            if (x != parent[x]) {
+                parent[x] = find(parent[x]);
+            }
+            return parent[x];
+        }
+    }
+}
