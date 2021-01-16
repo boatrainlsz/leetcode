836e807d13d55d84d98cd2b772ddf264feb6b47b
boatrain <yinger521999@gmail.com>
2021-01-16 21:33:24 +0800

    接雨水暴力解法

--- src/com/boatrain/interview/microsoft/TrappingRainWater.java
+++ src/com/boatrain/interview/microsoft/TrappingRainWater.java
@@ -11,6 +11,18 @@ public class TrappingRainWater {
     }
 
     public int trap(int[] height) {
-        return 0;
+        int ans = 0;
+        int size = height.length;
+        for (int i = 1; i < size - 1; i++) {
+            int maxLeft = 0, maxRight = 0;
+            for (int j = i; j >= 0; j--) {
+                maxLeft = Math.max(maxLeft, height[j]);
+            }
+            for (int j = i; j < size; j++) {
+                maxRight = Math.max(maxRight, height[j]);
+            }
+            ans += Math.min(maxLeft, maxRight) - height[i];
+        }
+        return ans;
     }
 }
