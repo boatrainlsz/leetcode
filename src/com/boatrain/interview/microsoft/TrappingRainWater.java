23a8c4966520fc5cfaf1d73559b5a5b91946ad6b
boatrain <yinger521999@gmail.com>
2021-01-16 21:47:22 +0800

    接雨水O(N)解法

--- src/com/boatrain/interview/microsoft/TrappingRainWater.java
+++ src/com/boatrain/interview/microsoft/TrappingRainWater.java
@@ -25,4 +25,31 @@ public class TrappingRainWater {
         }
         return ans;
     }
+
+    /**
+     * O(n)的解法
+     *
+     * @param height
+     * @return
+     */
+    public int trapBetter(int[] height) {
+        if (height == null || height.length == 0)
+            return 0;
+        int ans = 0;
+        int size = height.length;
+        int[] leftMax = new int[size];
+        int[] rightMax = new int[size];
+        leftMax[0] = height[0];
+        for (int i = 1; i < size; i++) {
+            leftMax[i] = Math.max(height[i], leftMax[i - 1]);
+        }
+        rightMax[size - 1] = height[size - 1];
+        for (int i = size - 2; i >= 0; i--) {
+            rightMax[i] = Math.max(height[i], rightMax[i + 1]);
+        }
+        for (int i = 1; i < size - 1; i++) {
+            ans += Math.min(leftMax[i], rightMax[i]) - height[i];
+        }
+        return ans;
+    }
 }
