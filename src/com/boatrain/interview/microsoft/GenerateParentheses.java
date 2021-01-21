f36c7746b99e0c7f0e1159e4c2a412af9d2a8193
boatrain <yinger521999@gmail.com>
2021-01-21 21:23:58 +0800

    括号生成

---
+++ src/com/boatrain/interview/microsoft/GenerateParentheses.java
@@ -0,0 +1,18 @@
+package com.boatrain.interview.microsoft;
+
+import java.util.List;
+
+/**
+ * https://leetcode-cn.com/problems/generate-parentheses/
+ */
+public class GenerateParentheses {
+    public static void main(String[] args) {
+        GenerateParentheses solution = new GenerateParentheses();
+        System.out.println(solution.generateParenthesis(3));
+    }
+
+    public List<String> generateParenthesis(int n) {
+
+        return null;
+    }
+}
