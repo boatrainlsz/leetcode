7c9a707b569a100f34a005d09c6f966d207bd6de
boatrain <yinger521999@gmail.com>
2021-01-27 22:30:54 +0800

    并擦回家

---
+++ src/com/boatrain/unionfind/RemoveMaxNumberOfEdges.java
@@ -0,0 +1,104 @@
+package com.boatrain.unionfind;
+
+import java.util.Arrays;
+
+/**
+ * https://leetcode-cn.com/problems/remove-max-number-of-edges-to-keep-graph-fully-traversable/
+ */
+public class RemoveMaxNumberOfEdges {
+    public static void main(String[] args) {
+        RemoveMaxNumberOfEdges solution = new RemoveMaxNumberOfEdges();
+        int[][] edges = {{3, 1, 2}, {3, 2, 3}, {1, 1, 3}, {1, 2, 4}, {1, 1, 2}, {2, 3, 4}};
+        System.out.println(solution.maxNumEdgesToRemove(4, edges));
+    }
+
+    public int maxNumEdgesToRemove(int n, int[][] edges) {
+        UnionFind ufa = new UnionFind(n);
+        UnionFind ufb = new UnionFind(n);
+        int ans = 0;
+
+        // 节点编号改为从 0 开始
+        for (int[] edge : edges) {
+            --edge[1];
+            --edge[2];
+        }
+
+        // 公共边
+        for (int[] edge : edges) {
+            if (edge[0] == 3) {
+                if (!ufa.unite(edge[1], edge[2])) {
+                    ++ans;
+                } else {
+                    ufb.unite(edge[1], edge[2]);
+                }
+            }
+        }
+
+        // 独占边
+        for (int[] edge : edges) {
+            if (edge[0] == 1) {
+                // Alice 独占边
+                if (!ufa.unite(edge[1], edge[2])) {
+                    ++ans;
+                }
+            } else if (edge[0] == 2) {
+                // Bob 独占边
+                if (!ufb.unite(edge[1], edge[2])) {
+                    ++ans;
+                }
+            }
+        }
+
+        if (ufa.setCount != 1 || ufb.setCount != 1) {
+            return -1;
+        }
+        return ans;
+    }
+}
+
+// 并查集模板
+class UnionFind {
+    int[] parent;
+    int[] size;
+    int n;
+    // 当前连通分量数目
+    int setCount;
+
+    public UnionFind(int n) {
+        this.n = n;
+        this.setCount = n;
+        this.parent = new int[n];
+        this.size = new int[n];
+        Arrays.fill(size, 1);
+        for (int i = 0; i < n; ++i) {
+            parent[i] = i;
+        }
+    }
+
+    public int findset(int x) {
+        return parent[x] == x ? x : (parent[x] = findset(parent[x]));
+    }
+
+    public boolean unite(int x, int y) {
+        x = findset(x);
+        y = findset(y);
+        if (x == y) {
+            return false;
+        }
+        if (size[x] < size[y]) {
+            int temp = x;
+            x = y;
+            y = temp;
+        }
+        parent[y] = x;
+        size[x] += size[y];
+        --setCount;
+        return true;
+    }
+
+    public boolean connected(int x, int y) {
+        x = findset(x);
+        y = findset(y);
+        return x == y;
+    }
+}
