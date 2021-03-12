caf22b9f4112197e15c26c6358fe1b7486b780dd
boatrain <yinger521999@gmail.com>
2021-03-12 17:15:18 +0800

    基本计算器

--- src/com/boatrain/stackandqueue/BasicCalculatorII.java
+++ src/com/boatrain/stackandqueue/BasicCalculatorII.java
@@ -5,7 +5,7 @@ import java.util.Stack;
 public class BasicCalculatorII {
     public static void main(String[] args) {
         BasicCalculatorII basicCalculatorII = new BasicCalculatorII();
-        int calculate = basicCalculatorII.calculate("1*2+4-25/5");
+        int calculate = basicCalculatorII.calculateWithoutStack("1*2+4-25/5");
         System.out.println(calculate);
     }
 
@@ -37,6 +37,33 @@ public class BasicCalculatorII {
         while (!stack.isEmpty()) {
             ans += stack.pop();
         }
+        return ans;
+    }
+
+    public int calculateWithoutStack(String s) {
+        int ans = 0;
+        int n = s.length();
+        int currentNumber = 0, lastNumber = 0;
+        char operation = '+';
+        for (int i = 0; i < n; i++) {
+            char c = s.charAt(i);
+            if (Character.isDigit(c)) {
+                currentNumber = (currentNumber * 10) + (c - '0');
+            }
+            if (!Character.isDigit(c) && !Character.isWhitespace(c) || i == n - 1) {
+                if (operation == '*') {
+                    lastNumber *= currentNumber;
+                } else if (operation == '/') {
+                    lastNumber /= currentNumber;
+                } else if (operation == '+' || operation == '-') {
+                    ans += lastNumber;
+                    lastNumber = operation == '+' ? currentNumber : -currentNumber;
+                }
+                operation = c;
+                currentNumber = 0;
+            }
+        }
+        ans += lastNumber;
         return ans;
     }
 }
