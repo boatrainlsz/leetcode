a51536f8ddf39665e976e7e66e5e007c78a723e1
Samuel <samuel@fanruan.com>
2021-10-06 23:07:47 +0800

    全排列II

---
+++ src/com/boatrain/backtrack/PermutationsII.java
@@ -0,0 +1,44 @@
+package com.boatrain.backtrack;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+
+public class PermutationsII {
+    public static void main(String[] args) {
+        PermutationsII solution = new PermutationsII();
+        int[] nums = {1, 1, 2};
+        System.out.println(solution.permuteUnique(nums));
+    }
+
+    List<List<Integer>> ans = new ArrayList<>();
+    HashSet<Integer> set = new HashSet<>();
+    HashMap<Integer, Integer> countMap = new HashMap<>();
+
+    public List<List<Integer>> permuteUnique(int[] nums) {
+        for (int num : nums) {
+            countMap.merge(num, 1, Integer::sum);
+            set.add(num);
+        }
+        dfs(nums, 0, new ArrayList<Integer>());
+        return ans;
+    }
+
+    private void dfs(int[] nums, int index, ArrayList<Integer> path) {
+        if (path.size() == nums.length) {
+            ans.add(new ArrayList(path));
+            return;
+        }
+        for (Map.Entry<Integer, Integer> entry : countMap.entrySet()) {
+            if (entry.getValue() > 0) {
+                path.add(entry.getKey());
+                countMap.merge(entry.getKey(), -1, Integer::sum);
+                dfs(nums, index + 1, path);
+                path.remove(path.size() - 1);
+                countMap.merge(entry.getKey(), 1, Integer::sum);
+            }
+        }
+    }
+}
