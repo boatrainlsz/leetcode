01173d4abc13bf2273a39e08c9cf0e480a8918df
Samuel <samuel@fanruan.com>
2021-10-06 19:33:42 +0800

    组合总和

--- src/com/boatrain/backtrack/CombinationSum.java
+++ src/com/boatrain/backtrack/CombinationSum.java
@@ -4,12 +4,12 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class CombinationSum {
-    List<List<Integer>> ans = new ArrayList<>();
-
     public static void main(String[] args) {
         CombinationSum solution = new CombinationSum();
         System.out.println(solution.combinationSum(new int[]{2, 3, 5}, 8));
     }
+
+    List<List<Integer>> ans = new ArrayList<>();
 
     public List<List<Integer>> combinationSum(int[] candidates, int target) {
         List<Integer> path = new ArrayList<>();
@@ -26,11 +26,9 @@ public class CombinationSum {
             return;
         }
         if (index == candidates.length) return;
-        for (int i = 0; candidates[index] * i <= target; i++) {
-            find(candidates, target - candidates[index] * i, index + 1, path);
-            path.add(candidates[index]);
-        }
-        for (int i = 0; candidates[index] * i <= target; i++) {
+        for (int i = index; i < candidates.length; i++) {
+            path.add(candidates[i]);
+            find(candidates, target - candidates[i], i, path);
             path.remove(path.size() - 1);
         }
     }
