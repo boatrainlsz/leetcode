c3001dd68131cd0b2ffae6285eab0e3718dc5260
Samuel <samuel@fanruan.com>
2021-10-10 21:53:40 +0800

    dijkstra最短路算法

--- src/com/boatrain/graph/NetworkDelayTime.java
+++ src/com/boatrain/graph/NetworkDelayTime.java
@@ -38,11 +38,13 @@ public class NetworkDelayTime {
         }
         confirmed = new boolean[graph.size()];
         distance = new int[graph.size()];
+        //初始化，k到所有节点最短距离都是正无穷，自身最短距离为0
         Arrays.fill(distance, Integer.MAX_VALUE);
         distance[k] = 0;
         dijkstra(k);
         int ans = Integer.MIN_VALUE;
         for (int i = 1; i < distance.length; i++) {
+            //有一个还没有确定，就说明不可达
             if (distance[i] == Integer.MAX_VALUE) return -1;
             ans = Math.max(distance[i], ans);
         }
@@ -50,7 +52,7 @@ public class NetworkDelayTime {
     }
 
     private void dijkstra(int src) {
-        //1.求出当前未确定最短路的节点的距离
+        //1.求出当前未确定最短路的节点的距离的最小值
         while (true) {
             int curDistance = Integer.MAX_VALUE, curNode = -1;
             for (int node = 1; node <= n; node++) {
@@ -59,9 +61,11 @@ public class NetworkDelayTime {
                     curNode = node;
                 }
             }
+            //这个最小值就是k->curNode的最短路大小，因为其他未确定节点的最短路都比他大
+            //从其他节点绕路来到k，距离只会更大
             if (curNode == -1) break;
             confirmed[curNode] = true;
-            //2.更新
+            //2.根据curNode更新curNode的邻接节点
             Map<Integer, Integer> adjs = graph.get(curNode);
             for (Map.Entry<Integer, Integer> entry : adjs.entrySet()) {
                 Integer adj = entry.getKey();
