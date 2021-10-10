6632dbc81bba3bb7c7eaa42e5beaf8d6f55bc906
Samuel <samuel@fanruan.com>
2021-10-10 21:39:00 +0800

    dijkstra最短路算法

--- src/com/boatrain/graph/NetworkDelayTime.java
+++ src/com/boatrain/graph/NetworkDelayTime.java
@@ -1,10 +1,16 @@
 package com.boatrain.graph;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+/**
+ * dijkstra最短路算法
+ * k->t的传播时间就是k->t的最短路距离
+ * 求出k到所有点的最短路，求最大值即是答案
+ */
 public class NetworkDelayTime {
     public static void main(String[] args) {
         NetworkDelayTime solution = new NetworkDelayTime();
@@ -12,45 +18,58 @@ public class NetworkDelayTime {
         System.out.println(solution.networkDelayTime(times, 4, 2));
     }
 
-    List<HashMap<Integer, Integer>> graph = new ArrayList<>();
-    boolean[] visited;
-    int ans = -1;
+    List<Map<Integer, Integer>> graph = new ArrayList<>();
+    //是否已经确定了k到此节点的最短路
+    boolean[] confirmed;
+    //k到此节点的最短路
+    int[] distance;
+    int n;
 
     public int networkDelayTime(int[][] times, int n, int k) {
-        for (int i = 1; i <= n + 1; i++) {
+        this.n = n;
+        for (int i = 0; i < n + 1; i++) {
             graph.add(new HashMap<>());
         }
         for (int i = 0; i < times.length; i++) {
-            graph.get(times[i][0]).put(times[i][1], times[i][2]);
+            int src = times[i][0];
+            int dst = times[i][1];
+            int cost = times[i][2];
+            graph.get(src).put(dst, cost);
         }
-        visited = new boolean[graph.size()];
-        visited[k] = true;
-        dfs(k, 0);
-        for (int i = 1; i < visited.length; i++) {
-            if (!visited[i]) return -1;
-
+        confirmed = new boolean[graph.size()];
+        distance = new int[graph.size()];
+        Arrays.fill(distance, Integer.MAX_VALUE);
+        distance[k] = 0;
+        dijkstra(k);
+        int ans = Integer.MIN_VALUE;
+        for (int i = 1; i < distance.length; i++) {
+            if (distance[i] == Integer.MAX_VALUE) return -1;
+            ans = Math.max(distance[i], ans);
         }
         return ans;
     }
 
-    private void dfs(int node, int curCost) {
-        boolean reachEnd = true;
-        HashMap<Integer, Integer> adjs = graph.get(node);
-        for (Integer adj : adjs.keySet()) {
-            if (!visited[adj]) {
-                reachEnd = false;
-                break;
+    private void dijkstra(int src) {
+        //1.求出当前未确定最短路的节点的距离
+        while (true) {
+            int curDistance = Integer.MAX_VALUE, curNode = -1;
+            for (int node = 1; node <= n; node++) {
+                if (!confirmed[node] && distance[node] < curDistance) {
+                    curDistance = distance[node];
+                    curNode = node;
+                }
             }
-        }
-        if (reachEnd) {
-            ans = Math.max(curCost, ans);
-        }
-        for (Map.Entry<Integer, Integer> entry : adjs.entrySet()) {
-            Integer adj = entry.getKey();
-            if (!visited[adj]) {
-                visited[adj] = true;
-                Integer cost = entry.getValue();
-                dfs(adj, cost+curCost);
+            if (curNode == -1) break;
+            confirmed[curNode] = true;
+            //2.更新
+            Map<Integer, Integer> adjs = graph.get(curNode);
+            for (Map.Entry<Integer, Integer> entry : adjs.entrySet()) {
+                Integer adj = entry.getKey();
+                if (!confirmed[adj]) {
+                    if (distance[adj] > distance[curNode] + entry.getValue()) {
+                        distance[adj] = distance[curNode] + entry.getValue();
+                    }
+                }
             }
         }
     }
