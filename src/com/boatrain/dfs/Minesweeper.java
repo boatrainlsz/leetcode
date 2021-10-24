00d734c3a336fe845e0b205efa20eff085280b51
Samuel <samuel@fanruan.com>
2021-10-24 22:03:02 +0800

    扫雷

---
+++ src/com/boatrain/dfs/Minesweeper.java
@@ -0,0 +1,62 @@
+package com.boatrain.dfs;
+
+public class Minesweeper {
+
+    int[][] directions = new int[][]{
+            {0, 1}, {0, -1}, {1, 0}, {-1, 0},
+            {-1, -1}, {-1, 1}, {1, -1}, {1, 1}
+    };
+    int maxX;
+    int maxY;
+    boolean[][] visited;
+
+    public char[][] updateBoard(char[][] board, int[] click) {
+        maxX = board.length;
+        maxY = board[0].length;
+        visited = new boolean[maxX][maxY];
+        int x = click[0];
+        int y = click[1];
+        if (board[x][y] == 'M') {
+            board[x][y] = 'X';
+            return board;
+        }
+        if (board[x][y] == 'E') {
+            dfs(board, x, y);
+            return board;
+        }
+        return board;
+    }
+
+    private void dfs(char[][] board, int x, int y) {
+        visited[x][y] = true;
+        int mineCountByMySide = mineCountByMySide(board, x, y);
+        if (mineCountByMySide != 0) {
+            board[x][y] = (char) (mineCountByMySide + 48);
+        } else {
+            board[x][y] = 'B';
+            for (int[] direction : directions) {
+                int newX = x + direction[0];
+                int newY = y + direction[1];
+                if (inBoard(newX, newY) && !visited[newX][newY]) {
+                    dfs(board, newX, newY);
+                }
+            }
+        }
+    }
+
+    private int mineCountByMySide(char[][] board, int x, int y) {
+        int count = 0;
+        for (int[] direction : directions) {
+            int newX = x + direction[0];
+            int newY = y + direction[1];
+            if (inBoard(newX, newY) && board[newX][newY] == 'M') {
+                count++;
+            }
+        }
+        return count;
+    }
+
+    private boolean inBoard(int x, int y) {
+        return x >= 0 && x < maxX && y >= 0 && y < maxY;
+    }
+}
