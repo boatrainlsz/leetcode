c77390c8f0ef8e3d24e2b58eecdfc0e1c4199a0b
Samuel <samuel@fanruan.com>
2022-01-16 16:07:09 +0800

    解决智力问题

--- src/com/boatrain/dynamicprogramming/MostPoints.java
+++ src/com/boatrain/dynamicprogramming/MostPoints.java
@@ -8,9 +8,11 @@ public class MostPoints {
         var n = questions.length;
         var f = new long[n + 1];
         for (int i = 0; i < n; i++) {
+            //不解决i问题，更新i+1
             f[i + 1] = Math.max(f[i + 1], f[i]);
             var q = questions[i];
             var j = Math.min(i + q[1] + 1, n);
+            //解决i问题，更新i+q[1]+1
             f[j] = Math.max(f[j], f[i] + q[0]);
         }
         return f[n];
