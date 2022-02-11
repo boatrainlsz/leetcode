a2fa51b84d893cc00a2ca41ed7e1bd6b20f32e36
Samuel <samuel@fanruan.com>
2022-02-11 21:59:26 +0800

    剑指 Offer 53 - II. 0～n-1中缺失的数字

---
+++ src/com/boatrain/array/MissingNumber.java
@@ -0,0 +1,11 @@
+package com.boatrain.array;
+
+import java.util.Arrays;
+
+public class MissingNumber {
+    public int missingNumber(int[] nums) {
+        int sum = Arrays.stream(nums).sum();
+        int n = nums.length;
+        return n * (n + 1) / 2 - sum;
+    }
+}
