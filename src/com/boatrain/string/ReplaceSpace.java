b1192c9c3ab731deb1940a6abec63e4b3dabed27
Samuel <samuel@fanruan.com>
2022-02-16 21:32:56 +0800

    替换空格

---
+++ src/com/boatrain/string/ReplaceSpace.java
@@ -0,0 +1,10 @@
+package com.boatrain.string;
+
+/**
+ * https://leetcode-cn.com/problems/ti-huan-kong-ge-lcof/
+ */
+public class ReplaceSpace {
+    public String replaceSpace(String s) {
+        return s.replace(" ", "%20");
+    }
+}
