bc00dba879e89e9a74b391f80ad808a3ca75bcf8
Samuel <samuel@fanruan.com>
2022-02-18 21:22:58 +0800

    1791. 找出星型图的中心节点

--- src/com/boatrain/hash/FindCenter.java
+++ src/com/boatrain/hash/FindCenter.java
@@ -1,18 +1,24 @@
 package com.boatrain.hash;
 
-import java.util.Comparator;
-import java.util.HashMap;
-
 /**
  * https://leetcode-cn.com/problems/find-center-of-star-graph/
  */
 class FindCenter {
     public int findCenter(int[][] edges) {
-        HashMap<Integer, Integer> countMap = new HashMap<>();
+        int n = edges.length + 1;
+        int[] count = new int[n + 1];
         for (int[] edge : edges) {
-            countMap.merge(edge[0], 1, Integer::sum);
-            countMap.merge(edge[1], 1, Integer::sum);
+            count[edge[0]]++;
+            count[edge[1]]++;
         }
-        return countMap.keySet().stream().max(Comparator.comparingInt(countMap::get)).get();
+        int max = Integer.MIN_VALUE;
+        int index = -1;
+        for (int i = 0; i < count.length; i++) {
+            if (max < count[i]) {
+                max = count[i];
+                index = i;
+            }
+        }
+        return index;
     }
 }
