aee62eb2cee2579007214ba24c4fc363100217d3
boatrain <yinger521999.com>
2022-05-08 21:33:53 +0800

    442. 数组中重复的数据

--- src/com/boatrain/hash/FindAllDuplicatesInAnArray.java
+++ src/com/boatrain/hash/FindAllDuplicatesInAnArray.java
@@ -14,6 +14,10 @@ public class FindAllDuplicatesInAnArray {
     }
 
     public List<Integer> findDuplicates(int[] nums) {
+        /**
+         * 如果没有重复，则整数n应该出现在位置n-1的地方，现在有重复，比如n重复了两次，那么按照下面这个算法，
+         * n-1这个地方会被标记两次，这样就能检测出
+         */
         List<Integer> ans = new ArrayList<>();
         for (int num : nums) {
             if (nums[Math.abs(num) - 1] < 0) {
