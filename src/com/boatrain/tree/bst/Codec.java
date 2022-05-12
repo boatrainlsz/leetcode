3dbde6d136a007e0d9b98f469fb8052d8c47feb7
boatrain <yinger521999@gmail.com>
2022-05-12 19:52:35 +0800

    剑指 Offer 31. 栈的压入、弹出序列

---
+++ src/com/boatrain/tree/bst/Codec.java
@@ -0,0 +1,67 @@
+package com.boatrain.tree.bst;
+
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Deque;
+import java.util.List;
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ * int val;
+ * TreeNode left;
+ * TreeNode right;
+ * TreeNode(int x) { val = x; }
+ * }
+ */
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode(int x) {
+        val = x;
+    }
+}
+
+public class Codec {
+    public String serialize(TreeNode root) {
+        List<Integer> list = new ArrayList<Integer>();
+        postOrder(root, list);
+        String str = list.toString();
+        return str.substring(1, str.length() - 1);
+    }
+
+    public TreeNode deserialize(String data) {
+        if (data.isEmpty()) {
+            return null;
+        }
+        String[] arr = data.split(", ");
+        Deque<Integer> stack = new ArrayDeque<Integer>();
+        int length = arr.length;
+        for (int i = 0; i < length; i++) {
+            stack.push(Integer.parseInt(arr[i]));
+        }
+        return construct(Integer.MIN_VALUE, Integer.MAX_VALUE, stack);
+    }
+
+    private void postOrder(TreeNode root, List<Integer> list) {
+        if (root == null) {
+            return;
+        }
+        postOrder(root.left, list);
+        postOrder(root.right, list);
+        list.add(root.val);
+    }
+
+    private TreeNode construct(int lower, int upper, Deque<Integer> stack) {
+        if (stack.isEmpty() || stack.peek() < lower || stack.peek() > upper) {
+            return null;
+        }
+        int val = stack.pop();
+        TreeNode root = new TreeNode(val);
+        root.right = construct(val, upper, stack);
+        root.left = construct(lower, val, stack);
+        return root;
+    }
+}
