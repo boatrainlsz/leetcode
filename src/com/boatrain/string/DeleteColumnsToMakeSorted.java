e70163f0ec575d7cc9a4efbfc47d2498eb78a1e0
boatrain <yinger521999@gmail.com>
2022-05-12 19:58:41 +0800

    944. 删列造序

---
+++ src/com/boatrain/string/DeleteColumnsToMakeSorted.java
@@ -0,0 +1,20 @@
+package com.boatrain.string;
+
+/**
+ * https://leetcode.cn/problems/delete-columns-to-make-sorted/
+ */
+public class DeleteColumnsToMakeSorted {
+    public int minDeletionSize(String[] strs) {
+        int count = 0;
+        int len = strs[0].length();
+        for (int i = 0; i < len; i++) {
+            for (int j = 0; j < strs.length - 1; j++) {
+                if (strs[j].charAt(i) > strs[j + 1].charAt(i)) {
+                    count++;
+                    break;
+                }
+            }
+        }
+        return count;
+    }
+}
