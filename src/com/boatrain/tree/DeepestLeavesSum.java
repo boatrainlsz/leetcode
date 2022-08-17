92278d0c45a65c09fb3336f96cd7cec3dc38d978
luosizhi <luosizhi@archeros.com>
2022-08-17 19:39:52 +0800

    层数最深叶子节点的和

---
+++ src/com/boatrain/tree/DeepestLeavesSum.java
@@ -0,0 +1,32 @@
+package com.boatrain.tree;
+
+/**
+ * https://leetcode.cn/problems/deepest-leaves-sum/
+ */
+public class DeepestLeavesSum {
+    int sum = 0;
+    int currentDeepestLevel = 0;
+
+    public int deepestLeavesSum(TreeNode root) {
+        int currentLevel = 0;
+        inOrder(root, currentLevel);
+        return sum;
+    }
+
+    private void inOrder(TreeNode node, int currentLevel) {
+        if (node == null) {
+            return;
+        }
+        currentLevel++;
+        if (currentLevel == currentDeepestLevel) {
+            sum += node.val;
+        } else if (currentLevel > currentDeepestLevel) {
+            currentDeepestLevel = currentLevel;
+            sum = node.val;
+        }
+        //左子树
+        inOrder(node.left, currentLevel);
+        //右子树
+        inOrder(node.right, currentLevel);
+    }
+}
